10c5adc7fe9cdaa20f12645653509b45b57bcb64
Johann Chen <johannchen@gmail.com>
2012-11-09 16:42:47 -0800

    add new question to mongolab

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,4 +2,19 @@
 
 bibleqnaApp.controller('MainCtrl', function($scope, Verse) {
   $scope.verses = Verse.query();
+
+  $scope.save = function() {
+    $scope.verses.push($scope.verse);
+    // save in mongo
+    Verse.save($scope.verse);
+    $scope.verse = {};
+  };
+
+  $scope.answer = function(verse) {
+   // save in mongo
+   console.log("id: " + verse._id);
+   var dbVerse = Verse.get({id: verse._id});
+   //verse.update(); 
+   console.log(dbVerse);
+  };
 });
--- app/scripts/services/Verse.js
+++ app/scripts/services/Verse.js
@@ -1,7 +1,16 @@
 'use strict';
 
 bibleqnaApp.factory('Verse', function($resource) {
-  return $resource('https://api.mongolab.com/api/1/databases/bible/collections/verses/:id',
-    { apiKey: '50983e52e4b0200e9ba50a55' }
+  var Verse = $resource('https://api.mongolab.com/api/1/databases/bible/collections/verses/:id',
+    { apiKey: '50983e52e4b0200e9ba50a55' }, {
+      update: { method: 'PUT' }
+    }
   );
+/*
+  Verse.prototype.update = function(cb) {
+    return Verse.update({id: this._id},
+      angular.extend({}, this, {_id:undefined}), cb);
+  };
+*/
+  return Verse;
 });
