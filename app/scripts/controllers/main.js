8da0c0dd3e88bdfba8fec19577f1901b4d9f3f30
johannchen <johannchen@gmail.com>
2012-12-29 22:23:00 -0800

    update answer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,20 +3,32 @@
 bibleqnaApp.controller('MainCtrl', function($scope, Verse) {
   $scope.verses = Verse.query();
 
-  $scope.save = function() {
-    $scope.verses.push($scope.verse);
+  $scope.submitQuestion = function() {
     // save in mongo
-    Verse.save($scope.verse);
+    Verse.save($scope.verse, function(verse) {
+      $scope.verses.unshift(verse);
+    });
     $scope.verse = {};
+    $scope.questionFormShowed = false;
   };
 
-  $scope.answer = function(verse) {
+  $scope.updateQuestion = function() {
+    var verse = this.verse;
+    verse.update(function() {
+      console.log("update question");
+    });
+    this.editMode = false;
+  };
+
+  $scope.submitAnswer = function() {
+    var verse = this.verse;
     if (verse.answers === undefined) {verse.answers = [];}
     verse.answers.push(this.newAnswer);
     verse.update(function() {
       console.log("answer question");
     });
     this.newAnswer = "";
+    this.answerFormShowed = false;
 
    // save in mongo
    //console.log("id: " + verse._id.$oid);
@@ -24,4 +36,33 @@ bibleqnaApp.controller('MainCtrl', function($scope, Verse) {
    //dbVerse.update(); 
    //console.log(dbVerse);
   };
+
+  $scope.removeAnswer = function() {
+    if (confirm("Are you sure to remove this answer?")) {
+      // get verse from parent scope.
+      var verse = this.$parent.verse;
+      var index = verse.answers.indexOf(this.answer);
+      // console.log("answer index: " + index);
+      verse.answers.splice(index, 1);
+      verse.update(function() {
+        console.log("remove answer");
+      });
+    };
+  };
+
+  $scope.updateAnswer = function() {
+    // get verse from parent scope.
+    var verse = this.$parent.verse;
+    
+    var index = verse.answers.indexOf(this.answer);    
+    verse.answers[index] = this.editedAnswer;
+    //this.answer = this.editedAnswer;
+    
+    verse.update(function() {
+      console.log("update answer");
+    });
+
+    this.editAnswerMode = false;
+  };
+
 });
